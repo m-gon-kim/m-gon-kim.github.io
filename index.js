ef5e44e02f7815c5d716a7783f16f0429687e9a5
김Myoung Gon <m_gon_kim@korea.ac.kr>
2024-09-22 01:48:10 +0900

    Update 2024
    Signed-off-by: 김Myoung Gon <m_gon_kim@korea.ac.kr>

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 function backgroundImg(obj, image){
 	$(obj).css("background-image","url('"+image+"')");
 }
+
+function ResearchReadMore(obj){
+	$(obj).parent().parent().parent().children(".researchDetailWrapper").css("display","block");
+	$(obj).css("display", "none");
+}
